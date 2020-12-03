40de8e889082420f8371f6d647dfb6dd64b187b1
1dpiho0120 <1dpiho0120@gmail.com>
2020-12-03 16:43:44 +0800

    CV檔案

---
+++ CV/影片_人臉偵測&特徵萃取.py
@@ -0,0 +1,109 @@
+'''          ### 影片__人臉偵測 & 特徵萃取 ###       '''
+
+# 載入所需的套件
+import dlib
+import cv2
+import numpy as np
+# github上一個很方便的圖像處理包，可以導入python，實現平移，旋轉，調整大小，骨架化等一些操作
+import imutils
+from imutils import face_utils      # 人臉特徵萃取套件
+
+
+# 使用 OpenCV 讀取影片檔案
+cap = cv2.VideoCapture('./video61.mp4')
+
+
+# 取得畫面尺寸
+'''
+通過cap.get(propID)訪問影片的某些功能，propID是0到18之間的數字，每個數字分別表示影片的屬性，例如：
+cv2.CAP_PROP_FRAME_WIDTH -> propID=3 | cv2.CAP_PROP_FRAME_HEIGHT -> propID=4
+分別可得到影片的寬度和高度。如果想修改為320x240, 只須在後面加入欲修改成的寬x高：
+width = cap.set(cv2.CAP_PROP_FRAME_WIDTH，320) | height = cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+'''
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+
+# 定義視訊編解碼器的型別
+'''
+VideoWriter_fourcc為視訊編解碼器。視訊編解碼器是指一個能夠對數位影片進行壓縮或者解壓縮的程式或者裝置。
+cv2.VideoWriter_fourcc(‘X’, ‘V’, ‘I’, ‘D’),該引數是MPEG-4編碼型別，
+以字尾*.avi, *.mp4等結尾的檔案有一部分是使用這些視訊編解碼器的，這邊使用 XVID 編碼
+'''
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+
+# 建立 VideoWriter 物件，輸出影片至 output.mp4，FPS 值為 20.0
+'''
+cv2.VideoWriter() -> 使用這個函數可以創建一個用於寫出影片文件的句柄
+第一個參數是指定輸出的檔名，例如：下列範例中的 output.mp4，
+第二個參數為指定 FourCC，即為先前第32行設定寫出影片的編碼格式
+第三個參數為 fps 影像偵率，FPS值 = 影格率，是用於測量顯示影格數的量度。一般來說FPS用於描述影片、電子繪圖或遊戲每秒播放多少影格
+第四個參數為先前取得的原影像大小
+'''
+out = cv2.VideoWriter('./專題/output.mp4', fourcc, 20.0, (width, height))
+
+
+# Dlib 的人臉偵測器 & 特徵萃取器
+'''Dlib 使用的人臉偵測演算法是以方向梯度直方圖（HOG）的特徵加上線性分類器（linear classifier）、
+影像金字塔（image pyramid）與滑動窗格（sliding window）來實作。'''
+detector = dlib.get_frontal_face_detector()
+
+'''
+shape_predictor_68_face_landmarks.dat 為訓練好的模型，須先下載存取在本地端
+(https://github.com/italojs/facial-landmarks-recognition-/blob/master/shape_predictor_68_face_landmarks.dat)
+'''
+predictor = dlib.shape_predictor("./shape_predictor_68_face_landmarks.dat")
+
+
+# 以迴圈從影片檔案讀取影格，並顯示出來
+'''
+在這個無窮迴圈中，每次呼叫 cap.read() 就會讀取一張畫面，
+其第一個傳回值 ret代表讀取成功與否（True 代表成功，False 代表失敗），而第二個傳回值 frame 就是影片的單張畫面。
+'''
+while(True):
+    ret, frame = cap.read()
+
+    '''
+    這裡我們改用 detector.run 來偵測人臉，它的第三個參數是指定分數的門檻值，
+    所有分數超過這個門檻值的偵測結果都會被輸出，而傳回的結果除了人臉的位置(face_rects)之外，演算的結果會有一個分數(scores)與子偵測器的編號(idx), 
+    此分數愈大, 表示愈接近人臉，分數愈低表示愈接近誤判。子偵測器的編號則可以用來判斷人臉的方向。
+    '''
+    # 偵測人臉
+    face_rects, scores, idx = detector.run(frame, 0)
+    #print(scores, idx)
+
+    # 取出所有偵測的結果
+    '''face_rects會回傳兩個tuple，為人臉偵測的方框 ex. [(633, 237), (1079, 683)]'''
+    for face in face_rects:
+        x1 = face.left()
+        y1 = face.top()
+        x2 = face.right()
+        y2 = face.bottom()
+
+        shape = predictor(frame, face)             # 在每一個單張畫面的人臉方框檢查特徵點
+        shape = face_utils.shape_to_np(shape)   # 把臉部特徵點座標轉化為數組 Numpy array
+
+        # 以方框標示偵測的人臉 (影像, 開始座標, 結束座標, 顏色, 線條寬度<正數為粗細，負數為填滿>, 反鋸齒線條)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 4, cv2.LINE_AA)
+
+        for (x, y) in shape:
+            # 以圓點標示特徵點 （圖像、圓心、半徑、顏色、線條寬度<正數為粗細，負數為填滿>, 反鋸齒線條）
+            cv2.circle(frame, (x, y), 2, (0, 0, 255), -1, cv2.LINE_AA)
+
+    # 寫入影格
+    out.write(frame)
+
+    # 顯示結果 (命名窗口名稱, 欲顯示的圖片)
+    video = cv2.imshow("Facial Landmarks", frame)
+
+    '''
+    如下面的判斷式——若使用者没有按下q键,就會持續等待(循環)，直到觸發後執行break跳出迴圈
+    0xFF是十六進制常數，二進制值為11111111，和後面的ASCII碼對照。ord(' ')可以將字符轉化為對應的整數(ASCII碼)
+    此動作是為了防止BUG
+    '''
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# 確保關閉彈跳的視窗
+cv2.destroyAllWindows("Facial Landmarks")
---
+++ CV/照片_表情辨識建模_dlib.py
@@ -0,0 +1,167 @@
+'''         Emotion Recognition using Facial Landmarks, Python, DLib and OpenCV         '''
+
+# 載入所需的套件
+import dlib
+import cv2
+import numpy as np
+import glob             # glob 是文件操作相關的套件
+import random
+import math
+import itertools
+from sklearn.svm import SVC             # Support Vector Classification
+import joblib    # 保存 sklearn 模型的函式庫，亦可使用 pickle
+
+
+# 定義情緒種類，dataset以以下資料夾分類
+emotions = ["anger", "happy", "neutral", "sad", "surprise"]
+
+
+# 定義字適應
+'''Contrast Limited Adaptive Histogram Equalization（自適應值方圖均衡化），是一種計算機圖像處理技術，用於提高圖像的對比度
+clipLimit參數表示對比度的大小，tileGridSize參數表示每次處理塊的大小 。'''
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+
+
+# 使用 Dlib 偵測人臉 & 特徵點萃取
+detector = dlib.get_frontal_face_detector(
+)                                     # 呼叫人臉偵測器
+predictor = dlib.shape_predictor(
+    "./一些Python程式/shape_predictor_68_face_landmarks.dat")     # 呼叫特徵點萃取器
+
+
+'''    ★  SVM w/ Linear Kernel ★ （以下kernel參數可調整再訓練）  '''
+
+'''kernel：參數選擇有RBF(高斯核), Linear(線性核函數)，Poly(多項式函數), Sigmoid(sigmoid核函數)
+   probability：是否啟用概率估計， tol：停止訓練的誤差值大小， '''
+clf = SVC(kernel='linear', probability=True, tol=1e-5, verbose=False)
+
+
+data = {}           # 將所有特徵點的值帶入字典   data['landmarks_vectorised'] = []
+
+
+# 定義一個function蒐集並建立所有圖片檔案名稱的list, 隨機將資料分成80/20
+def get_files(emotion):
+    files = glob.glob("./Dataset/5_CK+/%s/*" %
+                      emotion)      # 將所有檔案名稱回傳並轉為list型別
+    random.shuffle(files)                           # 將list裡面的檔案名稱順序洗牌
+    training = files[:int(len(files)*0.8)]          # 取得list前面80%的檔案做為訓練集
+    prediction = files[-int(len(files)*0.2):]       # 取得list後面20%的檔案做為測試集
+    return training, prediction
+
+
+# 定義偵測人臉和特徵點的function
+def get_landmarks(img):
+    face_rects = detector(img, 0)                   # face_rects 回傳結果為人臉方框的座標
+    for k, d in enumerate(face_rects):
+        shape = predictor(img, d)                   # 使用predictor類別萃取人臉特徵點
+
+        xlist = []
+        ylist = []
+        for i in range(1, 68):                      # 將特徵點(共68點) X和Y座標各別存入兩個list
+            xlist.append(float(shape.part(i).x))
+            ylist.append(float(shape.part(i).y))
+
+        xmean = np.mean(xlist)                   # 取得 xlist 矩陣的平均值
+        ymean = np.mean(ylist)                   # 取得 ylist 矩陣的平均值
+
+        # 每個特徵點的x座標與xmean相減以得到離均差(deviation from the mean)
+        xcentral = [(x-xmean) for x in xlist]
+        # 每個特徵點的y座標與ymean相減以得到離均差(deviation from the mean)
+        ycentral = [(y-ymean) for y in ylist]
+
+        landmarks_vectorised = []
+        # zip() 函數可以將對應的元素打包成一個個tuple以節約內存
+        for x, y, w, z in zip(xcentral, ycentral, xlist, ylist):
+            landmarks_vectorised.append(w)
+            # landmarks_vectorised = [xlist[0], ylist[0], xlist[1], ylsit[1]...]
+            landmarks_vectorised.append(z)
+            # np.asarry() 將input轉為矩陣
+            meannp = np.asarray((ymean, xmean))
+            coornp = np.asarray((z, w))
+            # linalg=linear algebra，norm則表示範數。範數是對向量（或者矩陣）的度量，是一個標量（scalar）。
+            dist = np.linalg.norm(coornp-meannp)        # 計算每個特徵點座標到臉中心點的距離
+            landmarks_vectorised.append(dist)
+            # math.atan2()回傳從原點到(x, y)點的線段與x軸正方向之間的平面角度(弧度值)
+            # 用鼻樑的角度修正臉傾斜所造成的誤差
+            landmarks_vectorised.append((math.atan2(y, x)*360)/2*math.pi)
+
+            # 我們得到 a. 每個特徵點到臉中心點的距離  b. 角度  -> 矩陣
+            data['landmarks_vectorised'] = landmarks_vectorised
+
+    # 若人臉偵測失敗，value為error
+    if len(face_rects) < 1:
+        data['landmarks_vectorised'] = "error"
+
+
+def make_sets():
+    training_data = []
+    training_labels = []
+    prediction_data = []
+    prediction_labels = []
+
+    # 在每個emotion資料夾做迴圈
+    for emotion in emotions:
+        print(" working on %s" % emotion)
+        training, prediction = get_files(emotion)
+
+        # 將數據附加到訓練集的list和測試集的list中，並生成標籤0〜6
+        for item in training:                   # 對訓練集的每一張照片做迴圈
+            img = cv2.imread(item)              # 讀取訓練集的照片
+            # 轉換為灰階照片
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            clahe_image = clahe.apply(gray)
+            get_landmarks(clahe_image)          # 取得特徵點的矩陣
+
+            # 若value為error， print出此照片沒有偵測到人臉
+            if data['landmarks_vectorised'] == "error":
+                print("no face detected on this one")
+
+            else:
+                # 若有偵測到人臉，將特徵點矩陣加入training_data
+                training_data.append(data['landmarks_vectorised'])
+                # 將對應的情緒標籤號碼加入training_labels
+                training_labels.append(emotions.index(emotion))
+
+        for item in prediction:                 # 對測試集的每一張照片做迴圈
+            img = cv2.imread(item)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            clahe_image = clahe.apply(gray)
+            get_landmarks(clahe_image)
+
+            if data['landmarks_vectorised'] == "error":
+                print("no face detected on this one")
+            else:
+                prediction_data.append(data['landmarks_vectorised'])
+                prediction_labels.append(emotions.index(emotion))
+
+    return training_data, training_labels, prediction_data, prediction_labels
+
+
+accuracy_lin = []
+
+# 將模型訓練十次
+for i in range(0, 10):
+    print("Making sets %s" % i)
+    training_data, training_labels, prediction_data, prediction_labels = make_sets()
+
+    # 將訓練集轉成numpy矩陣以放進分類器進行學習
+    npar_train = np.array(training_data)
+    npar_trainlabs = np.array(training_labels)
+    print("training SVM linear %s" % i)
+    clf.fit(npar_train, training_labels)     # 將訓練集帶進SVM進行訓練
+
+    # 使用score()函數取得準確率
+    print("getting accuracies %s" % i)
+    npar_pred = np.array(prediction_data)
+    pred_lin = clf.score(npar_pred, prediction_labels)
+    print("linear: ", pred_lin)
+    accuracy_lin.append(pred_lin)               # 將準確率加入accur_lin
+
+
+# 取得訓練十次的準確率
+print("Mean value lin svm: %s" % np.mean(accuracy_lin))
+
+
+# 儲存訓練好的模型
+'''前面的clf為我們上面的svm.SVC()，後面的clf.pkl為檔名'''
+joblib.dump(clf, './一些Python程式/clf_4.pkl')
