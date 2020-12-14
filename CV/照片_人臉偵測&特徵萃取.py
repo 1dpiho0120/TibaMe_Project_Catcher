924dd1a6ea87c5166b06af2ff03eee3658ede946
1dpiho0120 <1dpiho0120@gmail.com>
2020-12-14 18:10:02 +0800

    修正部份code

--- CV/照片_人臉偵測&特徵萃取.py
+++ CV/照片_人臉偵測&特徵萃取.py
@@ -3,25 +3,26 @@
 # 載入所需的套件
 import dlib
 import cv2
-import numpy as np
 # github上一個很方便的圖像處理包，可以導入python，實現平移，旋轉，調整大小，骨架化等一些操作
 import imutils
-from imutils import face_utils      # 人臉特徵萃取套件
+from imutils import face_utils
 
 
 # 使用 OpenCV 讀取照片圖檔
-img = cv2.imread('./images2.jpg')
-img = imutils.resize(img, width=1280)     # 縮小圖片，亦可直接使用原圖
+img = cv2.imread('/media/hoho/Transcend/備份/專題/CV/dataset_test/pohan.jpg')
+img = imutils.resize(img, width=640)     # 縮小圖片，亦可直接使用原圖
 
 
 # 呼叫Dlib 的人臉偵測器 & 特徵萃取器
-'''Dlib 使用的人臉偵測演算法是以方向梯度直方圖（HOG）的特徵加上線性分類器（linear classifier）、影像金字塔（image pyramid）與滑動窗格（sliding window）來實作。'''
+'''Dlib 使用的人臉偵測演算法是以方向梯度直方圖（HOG）的特徵加上線性分類器（linear classifier）、
+影像金字塔（image pyramid）與滑動窗格（sliding window）來實作。'''
 detector = dlib.get_frontal_face_detector()
 
 '''dlib.shape_predictor具有預訓練的地標檢測器，dlib用於估計68個座標(x, y)的位置，這些座標映射在人臉上的面部點
 shape_predictor_68_face_landmarks.dat 為訓練好的模型，須先下載存取在本地端
 (https://github.com/italojs/facial-landmarks-recognition-/blob/master/shape_predictor_68_face_landmarks.dat)'''
-predictor = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
+predictor = dlib.shape_predictor(
+    '/media/hoho/Transcend/備份/專題/CV/shape_predictor_68_face_landmarks.dat')
 
 
 # 偵測人臉
@@ -39,6 +40,7 @@ for face in face_rects:
 
     # predictor(圖像, 人臉偵測方框座標) 標誌出臉部區域的特徵
     shape = predictor(img, face)
+
     # 將面部landmarks標誌(x, y)座標轉換成NumPy陣列
     shape = face_utils.shape_to_np(shape)
 
@@ -48,9 +50,6 @@ for face in face_rects:
     for (x, y) in shape:
         # 以圓點標示特徵點 （圖像、圓心、半徑、顏色、線條寬度<正數為粗細，負數為填滿>, 反鋸齒線條）
         cv2.circle(img, (x, y), 2, (0, 0, 255), -1, cv2.LINE_AA)
-
-    # 寫入照片
-    cv2.imwrite('Facial Landmarks'', img)
 
 
 # 顯示結果 (命名窗口名稱, 欲顯示的圖片)
